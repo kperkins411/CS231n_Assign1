0256102125f404185342a278f3167ee2ba218b8c
Keith <kperkins411@gmail.com>
2016-05-09 20:13:55 -0400

    Added smart W1 weight matrix initialization and dropout

--- classifiers/neural_net.py
+++ classifiers/neural_net.py
@@ -19,7 +19,7 @@ class TwoLayerNet(object):
   The outputs of the second fully-connected layer are the scores for each class.
   """
 
-  def __init__(self, input_size, hidden_size, output_size, std=1e-4):
+  def __init__(self, input_size, hidden_size, output_size, std=1e-4, use_smart_w1_init = True):
     """
     Initialize the model. Weights are initialized to small random values and
     biases are initialized to zero. Weights and biases are stored in the
@@ -36,119 +36,20 @@ class TwoLayerNet(object):
     - output_size: The number of classes C.
     """
     self.params = {}
-    #reLU divide by sqrt(2/fan_in)  works well but kills structure in visualized W1 matrix
-    self.params['W1'] = std * np.random.randn(input_size, hidden_size)/mth.sqrt(2.0/input_size)
-    #self.params['W1'] = std * np.random.randn(input_size, hidden_size)   #orig use this to get same answer as ipynb 
-    self.params['b1'] = np.zeros(hidden_size)
-    #divide by sqrt(fan_in) for non reLu  works well but kills structure in visualized W1 matrix
-    self.params['W2'] = std * np.random.randn(hidden_size, output_size)/mth.sqrt(hidden_size)
-    #self.params['W2'] = std * np.random.randn(hidden_size, output_size)   #orig use this to get same answer as ipynb
+    if use_smart_w1_init == True:
+        #reLU divide by sqrt(2/fan_in)  works well but kills structure in visualized W1 matrix
+        self.params['W1'] = std * np.random.randn(input_size, hidden_size)/mth.sqrt(2.0/input_size)
+        
+        #divide by sqrt(fan_in) for non reLu  works well but kills structure in visualized W1 matrix
+        self.params['W2'] = std * np.random.randn(hidden_size, output_size)/mth.sqrt(hidden_size)
+    else: 
+        self.params['W1'] = std * np.random.randn(input_size, hidden_size)   #orig use this to get same answer as ipynb 
+        self.params['W2'] = std * np.random.randn(hidden_size, output_size)   #orig use this to get same answer as ipynb
+
+    self.params['b1'] = np.zeros(hidden_size)   
     self.params['b2'] = np.zeros(output_size)
 
-
-  def loss_other(self, X, y=None, reg=0.0):
-    """
-    Compute the loss and gradients for a two layer fully connected neural
-    network.
-    Inputs:
-    - X: Input data of shape (N, D). Each X[i] is a training sample.
-    - y: Vector of training labels. y[i] is the label for X[i], and each y[i] is
-      an integer in the range 0 <= y[i] < C. This parameter is optional; if it
-      is not passed then we only return scores, and if it is passed then we
-      instead return the loss and gradients.
-    - reg: Regularization strength.
-    Returns:
-    If y is None, return a matrix scores of shape (N, C) where scores[i, c] is
-    the score for class c on input X[i].
-    If y is not None, instead return a tuple of:
-    - loss: Loss (data loss and regularization loss) for this batch of training
-      samples.
-    - grads: Dictionary mapping parameter names to gradients of those parameters
-      with respect to the loss function; has the same keys as self.params.
-    """
-    # Unpack variables from the params dictionary
-    W1, b1 = self.params['W1'], self.params['b1']
-    W2, b2 = self.params['W2'], self.params['b2']
-    N, D = X.shape
-
-    # Compute the forward pass
-    scores = None
-    #############################################################################
-    # TODO: Perform the forward pass, computing the class scores for the input. #
-    # Store the result in the scores variable, which should be an array of      #
-    # shape (N, C).                                                             #
-    #############################################################################
-
-    z1 = X.dot(W1) + b1.T
-    a1 = relu(z1)
-    z2 = a1.dot(W2) + b2.T
-    scores = z2
-
-    #############################################################################
-    #                              END OF YOUR CODE                             #
-    #############################################################################
-
-    # If the targets are not given then jump out, we're done
-    if y is None:
-      return scores
-
-    #############################################################################
-    # TODO: Finish the forward pass, and compute the loss. This should include  #
-    # both the data loss and L2 regularization for W1 and W2. Store the result  #
-    # in the variable loss, which should be a scalar. Use the Softmax           #
-    # classifier loss. So that your results match ours, multiply the            #
-    # regularization loss by 0.5                                                #
-    #############################################################################
-    num_train = X.shape[0]
-
-    scores -= np.max(scores,axis=1).reshape(num_train,1)
-    P = np.exp(scores)/np.reshape(np.sum(np.exp(scores),axis=1),(num_train,1))
-
-    loss = -np.sum(np.log(P[(range(num_train),y)]))
-    loss /= num_train
-    loss += 0.5 * reg * (np.sum(W1*W1) + np.sum(W2*W2))
-
-    #############################################################################
-    #                              END OF YOUR CODE                             #
-    #############################################################################
-
-    # Backward pass: compute gradients
-    grads = {}
-    #############################################################################
-    # TODO: Compute the backward pass, computing the derivatives of the weights #
-    # and biases. Store the results in the grads dictionary. For example,       #
-    # grads['W1'] should store the gradient on W1, and be a matrix of same size #
-    #############################################################################
-
-    P[(range(num_train),y)] = P[(range(num_train),y)] - 1
-
-    # loss differentiation at ouput layer
-    delta2 = P / num_train
-
-    # differentiation here is just letting value to np.dot(delta2,W2.T) z2>0
-    # this is achieved by dot product
-    delta1 = np.dot(delta2,W2.T) * ((z1 >= 0).astype(float))
-
-    dW2 = np.dot(a1.T,delta2)
-    dW1 = np.dot(X.T,delta1)
-
-    dW1 += reg * W1
-    dW2 += reg * W2
-
-    # # Same as matrix multiplication with 1-vector, chain rule works out
-    db1 = np.sum(delta1, axis=0)
-    db2 = np.sum(delta2, axis=0)
-
-    # store gradients
-    grads = {}
-    grads["W1"] = dW1
-    grads["W2"] = dW2
-    grads["b1"] = db1
-    grads["b2"] = db2
-    return loss, grads
-
-
-  def loss(self, X, y=None, reg=0.0):
+  def loss(self, X, y=None, reg=0.0, dropout_prob = 1.0):
     """
     Compute the loss and gradients for a two layer fully connected neural
     network.
@@ -185,18 +86,22 @@ class TwoLayerNet(object):
     #############################################################################
     #try this way, saves creating vecclamp time and mem
     #compute input*hidden layer
+  
     z1 = X.dot(W1) + b1.T
     H1 = relu(z1)
 
-    #lets apply dropout
-    #p=1     #no dropout validation accuracy (val) = .386
-    p= .87   #val = .392
-    #p= .75  #val = .39
-    #p=.5    #val = .375
-    #p=.25   #val = .365
-  
-    U1 = (np.random.rand(*H1.shape) < p)/p
-    H1 *=U1
+    #lets apply dropout (if ==1 then no change)
+    if dropout_prob < 1.0:
+        #p=1     #no dropout validation accuracy (val) = .379
+        #p=.95  #val = .381
+        #p=.92  #val = .381
+        #p=.9   #val = .38
+        #p=.5    #val = .363
+        #p=.25   #val = 
+        
+        #U1 = np.random.binomial([np.ones(*H1.shape)],.5)* (1.0/(1-0.5))  # does not work
+        U1 = (np.random.rand(*H1.shape) < dropout_prob)/dropout_prob
+        H1 *=U1
     
     #now this mult the H1 (N,H) matrix by W2 (H,C) to get (N,C)
     #now get the raw scores
@@ -227,25 +132,20 @@ class TwoLayerNet(object):
     dW2 += reg * W2   #d( (1/2)*reg*W**2)/dw = reg*W 
     db2 = np.sum(dscores, axis=0, keepdims=True)
 
-    # W2 += -.5*reg*dW2
-    # b2 += -.5*reg*db2
-
     #get hidden layer gradient, d (3x)/dx = 3, if relu >0 then multiply 1 by coefficient
     dH1 = np.dot(dscores,W2.T) * ((z1 >= 0).astype(float))
     
-    #dont count dropped neuron contributions
-    dH1 *= U1
+    #dont count dropped neuron contributions (if ==1 then no change)
+    if dropout_prob < 1.0:
+        dH1 *= U1
     
     dW1 = np.dot(X.T, dH1)
-    # dW1 /=num_train
     dW1 += reg * W1
+    #dW1 /=num_train
 
     # print ("neural_net1 causes -7.30238520e-02")
     # print (dW1)
     db1 = np.sum(dH1, axis=0, keepdims=True)
-
-    # W1 += -.5*reg*dW1
-    # b1 += -.5*reg*db1
 
     # store gradients
     grads = {}
@@ -258,7 +158,7 @@ class TwoLayerNet(object):
   def train(self, X, y, X_val, y_val,
             learning_rate=1e-3, learning_rate_decay=0.95,
             reg=1e-5, num_iters=100,
-            batch_size=200, verbose=False):
+            batch_size=200, verbose=False, dropout_prob = 1):
     """
     Train this neural network using stochastic gradient descent.
 
@@ -296,12 +196,14 @@ class TwoLayerNet(object):
       indices = np.random.choice(num_train,batch_size, replace = False)
       X_batch = X[indices]
       y_batch = y[indices]
+        
+      #print "x_batch size " + str(X_batch.shape)
       #########################################################################
       #                             END OF YOUR CODE                          #
       #########################################################################
 
       # Compute loss and gradients using the current minibatch
-      loss, grads = self.loss(X_batch, y=y_batch, reg=reg)
+      loss, grads = self.loss(X_batch, y=y_batch, reg=reg, dropout_prob=dropout_prob)
       loss_history.append(loss)
 
       #########################################################################
@@ -368,5 +270,4 @@ class TwoLayerNet(object):
     ###########################################################################
 
     return y_pred
-
 
